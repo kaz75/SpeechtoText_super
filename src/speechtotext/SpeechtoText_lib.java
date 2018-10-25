8fb733e935d4a6019427c8e990496fc68cb9a441
kaz75 <43839310+kaz75@users.noreply.github.com>
2018-10-25 16:22:02 +0900

    Update SpeechtoText_lib.java

--- src/speechtotext/SpeechtoText_lib.java
+++ src/speechtotext/SpeechtoText_lib.java
@@ -2,7 +2,10 @@ package speechtotext;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionResults;
@@ -27,8 +30,32 @@ public class SpeechtoText_lib {
 			}
     }
     
+    
     public SpeechRecognitionResults getTranscript() {
         SpeechRecognitionResults transcript = service.recognize(options).execute();
         return transcript;
+    }
+    
+    public void getJson(SpeechRecognitionResults transcript) {
+	String s = String.valueOf(transcript);
+	   ObjectMapper mapper = new ObjectMapper();
+	   
+	   try {
+		JsonNode node = mapper.readTree(s);
+		for(int i = 0; i < node.get("results").size(); i++) {
+		//String text = node.get("results").toString();
+		//String text = node.get("results").get(0).toString();
+		//System.out.println(text);
+		String text = node.get("results").get(i).get("alternatives").get(0).get("transcript").toString();
+		System.out.println("transcript : "+ text);
+		double confidence = node.get("results").get(i).get("alternatives").get(0).get("confidence").asDouble();
+		System.out.println("confidence : "+ confidence);
+		
+		}
+		
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
     }
 }
