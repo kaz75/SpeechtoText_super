d10217699b472794707fc33ee58c9dc3ef852491
1618037 <1618037@cs16037>
2018-10-25 15:24:50 +0900

    a

--- src/speechtotext/SpeechtoText_main.java
+++ src/speechtotext/SpeechtoText_main.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionResults;
@@ -16,24 +18,33 @@ public class SpeechtoText_main {
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-			SpeechToText service = new SpeechToText();
-		    service.setUsernameAndPassword("99aca234-f422-4112-9a60-93f26a3dd22a", "4h44R1WPnKyr");
+			
+			SpeechtoText_lib slib = new SpeechtoText_lib(new File("audio/track06.mp3"));
+            SpeechRecognitionResults transcript = slib.getTranscript();
+            System.out.println(transcript);
 
-		    File audio = new File("audio/katou.wav");
-		    RecognizeOptions options = null;
-				   try {
-					   options = new RecognizeOptions.Builder()
-						.model("ja-JP_BroadbandModel")
-					    .audio(audio)
-				        .contentType(RecognizeOptions.ContentType.AUDIO_WAV)
-				        .build();
-				   } catch (FileNotFoundException e) {
+			
+					   String s = String.valueOf(transcript);
+					   ObjectMapper mapper = new ObjectMapper();
+					   
+					   try {
+						JsonNode node = mapper.readTree(s);
+						for(int i = 0; i < node.get("results").size(); i++) {
+						//String text = node.get("results").toString();
+						//String text = node.get("results").get(0).toString();
+						//System.out.println(text);
+						String text = node.get("results").get(i).get("alternatives").get(0).get("transcript").toString();
+						System.out.println("transcript : "+ text);
+						double confidence = node.get("results").get(i).get("alternatives").get(0).get("confidence").asDouble();
+						System.out.println("confidence : "+ confidence);
+						
+						}
+						
+					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-					   SpeechRecognitionResults transcript = service.recognize(options).execute();
-					   
-					   System.out.println(transcript);
+
 			}
 		private static void writeToFile(InputStream in, File file) {
 			   try {
