b3a842b12fa556742cf07464466ad1b08cba86cb
kaz75 <43839310+kaz75@users.noreply.github.com>
2018-10-25 16:21:03 +0900

    Update SpeechtoText_main.java

--- src/speechtotext/SpeechtoText_main.java
+++ src/speechtotext/SpeechtoText_main.java
@@ -22,43 +22,7 @@ public class SpeechtoText_main {
 			SpeechtoText_lib slib = new SpeechtoText_lib(new File("audio/track06.mp3"));
             SpeechRecognitionResults transcript = slib.getTranscript();
             System.out.println(transcript);
-
-			
-					   String s = String.valueOf(transcript);
-					   ObjectMapper mapper = new ObjectMapper();
-					   
-					   try {
-						JsonNode node = mapper.readTree(s);
-						for(int i = 0; i < node.get("results").size(); i++) {
-						//String text = node.get("results").toString();
-						//String text = node.get("results").get(0).toString();
-						//System.out.println(text);
-						String text = node.get("results").get(i).get("alternatives").get(0).get("transcript").toString();
-						System.out.println("transcript : "+ text);
-						double confidence = node.get("results").get(i).get("alternatives").get(0).get("confidence").asDouble();
-						System.out.println("confidence : "+ confidence);
-						
-						}
-						
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+            slib.getJson(transcript);
 
 			}
-		private static void writeToFile(InputStream in, File file) {
-			   try {
-			     OutputStream out = new FileOutputStream(file);
-			     byte[] buf = new byte[1024];
-			     int len;
-			     while ((len = in.read(buf)) > 0) {
-			       out.write(buf, 0, len);
-			     }
-			     out.close();
-			     in.close();
-			   } catch (Exception e) {
-			     e.printStackTrace();
-			   }
-			 }
-
 	}
